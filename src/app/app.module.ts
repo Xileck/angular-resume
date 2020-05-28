7aa76c18242b32117cbaf776ab7094aaf80d8cd9
Xileck <xileck@gmail.com>
2020-05-28 11:00:16 -0500

    Clean project with tailwind css

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainSectionComponent } from './main-section/main-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainSectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
